ae19cc6a84acce1360c42ee7aaa8b3080ab1410c
badlogic <badlogicgames@gmail.com>
2014-09-07 23:12:49 +0200

    added steering pr, tests and assets

--- tests/src/com/badlogic/gdx/tests/ai/TestStarter.java
+++ tests/src/com/badlogic/gdx/tests/ai/TestStarter.java
@@ -3,11 +3,10 @@ package com.badlogic.gdx.tests.ai;
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.badlogic.gdx.tests.ai.fsm.StateMachineTest;
 
 public class TestStarter {
 	public static void main (String[] argv) {
-		ApplicationListener test = new StateMachineTest();		
+		ApplicationListener test = new SteeringBehaviorTest();		
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.r = config.g = config.b = config.a = 8;
 		config.width = 960;
