ad0be6e44b298e2704ac7173c875d2288666a5a2
davebaol <davix7@yahoo.it>
2014-09-08 11:36:21 +0200

    Moved tests to the proper package.

---
+++ tests/src/com/badlogic/gdx/ai/tests/TestStarter.java
@@ -0,0 +1,16 @@
+package com.badlogic.gdx.ai.tests;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
+public class TestStarter {
+	public static void main (String[] argv) {
+		ApplicationListener test = new SteeringBehaviorTest();		
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.r = config.g = config.b = config.a = 8;
+		config.width = 960;
+		config.height = 600;
+		new LwjglApplication(test, config);
+	}
+}
