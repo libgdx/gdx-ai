61d245a0747cf401183410d1f9b3d27a44020789
davebaol <davix7@yahoo.it>
2014-11-30 18:47:44 +0100

    javadoc

--- gdx-ai/src/com/badlogic/gdx/ai/msg/Telegraph.java
+++ gdx-ai/src/com/badlogic/gdx/ai/msg/Telegraph.java
@@ -16,10 +16,13 @@
 
 package com.badlogic.gdx.ai.msg;
 
-
-/** @author davebaol */
+/** Any object implementing the {@code Telegraph} interface can act as the sender or the receiver of a {@link Telegram}.
+ * @author davebaol */
 public interface Telegraph {
 
+	/** Handles the telegram just received.
+	 * @param msg The telegram
+	 * @return {@code true} if the telegram has been successfully handled; {@code false} otherwise. */
 	public boolean handleMessage (Telegram msg);
 
 }
