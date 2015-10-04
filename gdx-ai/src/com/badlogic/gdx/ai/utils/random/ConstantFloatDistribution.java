65ce69ff055c87c23a3689145ef860be98c68267
davebaol <davix7@yahoo.it>
2015-10-04 22:14:13 +0200

    Ops... missing change :)

--- gdx-ai/src/com/badlogic/gdx/ai/utils/random/ConstantFloatDistribution.java
+++ gdx-ai/src/com/badlogic/gdx/ai/utils/random/ConstantFloatDistribution.java
@@ -22,6 +22,7 @@ public final class ConstantFloatDistribution extends FloatDistribution {
 	public static final ConstantFloatDistribution NEGATIVE_ONE = new ConstantFloatDistribution(-1);
 	public static final ConstantFloatDistribution ZERO = new ConstantFloatDistribution(0);
 	public static final ConstantFloatDistribution ONE = new ConstantFloatDistribution(1);
+	public static final ConstantFloatDistribution ZERO_POINT_FIVE = new ConstantFloatDistribution(.5f);
 
 	private final float value;
 
